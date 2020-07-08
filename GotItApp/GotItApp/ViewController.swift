e7029c0e673f6fff143248ab8558edd33f3a7b46
Ruslan <thepw@bk.ru>
2020-07-08 17:10:30 +0300

    Empty project has been added

---
+++ GotItApp/GotItApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GotItApp
+//
+//  Created by Admin on 08.07.2020.
+//  Copyright © 2020 team1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
