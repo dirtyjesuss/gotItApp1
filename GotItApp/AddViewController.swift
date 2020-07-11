5ec14804eb9ccfc81c572e4687784ac41d94d27f
Bulat <khabibullin23@mail.ru>
2020-07-11 15:08:39 +0300

    I created addviewcontroller

---
+++ GotItApp/AddViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddViewController.swift
+//  GotItApp
+//
+//  Created by Булат Хабибуллин on 11.07.2020.
+//  Copyright © 2020 team1. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("hello")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
